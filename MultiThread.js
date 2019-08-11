8f8d2f1479fb7291ce04e00ed6fef98cf5426a8e
Zack Dai <45176371+DaiZack@users.noreply.github.com>
2019-08-10 21:48:52 -0400

    Update MultiThread.js

--- MultiThread.js
+++ MultiThread.js
@@ -1,26 +1,39 @@
-function delay(one){
-    var p = new Promise(resolve => {setTimeout(resolve, 300)})
-    console.log(one)
-    return p
-  }
+const fs = require('fs')
+function myFunction(one){
+    return new Promise(resolve=>{
+        fs.readFile('geosample.csv',(err,data)=>{
+            var r = resolve(1)
+            console.log(one)
+            return r
+        })
+    })
 
-var a = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+  }
 
 async function loopInlimit(array,limit, callback){
     var box = [];
     var num = array.length/limit
-    for (var i=0;i<=num;i++){
+    
+    for (var i=0;i<=limit;i++){
         if(array.length>0){
-            box.push(array.splice(0,limit))
+            box.push(array.splice(0,num))
         }
     }
-    await box.map(async(smallbox)=>{
+    var ps = await box.map(async(smallbox)=>{
         // console.log(smallbox)
+        var ps = []
         for(one of smallbox){
             // console.log(one)
-            await callback(one)
+            var k = await callback(one)
+            ps.push(k)
         }
+        console.log('small box done') // do something when a thread done!
+        return ps
     })
+    await Promise.all(ps)
+    console.log('all done!') // do something when all threadsd one!
 }
 
-loopInlimit(a,4, delay)
+var alist = [1,2,3,4,5,6,7,8];
+numofThreads = 3
+loopInlimit(alist,numoThreads, myFunction)

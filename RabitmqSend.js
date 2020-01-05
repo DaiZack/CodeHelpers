68ecea6e6e0fc3e309ecfa532beb59e15e6ddb0b
Zack Dai <45176371+DaiZack@users.noreply.github.com>
2020-01-04 23:34:20 -0500

    Create RabitmqSend.js

---
+++ RabitmqSend.js
@@ -0,0 +1,26 @@
+const amqplib = require('amqplib');
+const fs=require('fs')
+const readline = require('readline');
+var q = 'tasks';
+var filename = 'test.csv'
+
+async function sendmsg(q,msg){
+    try{
+        var conn = await  amqplib.connect('amqp://localhost');
+        var ch = await conn.createChannel()
+        await ch.sendToQueue(q, Buffer.from(msg));
+        await ch.close();
+        console.log('msg send ', msg)
+        conn.close()
+    }catch(e){
+        console.log(e)
+    }
+}
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream(filename)
+});
+
+readInterface.on('line', function(line) {
+    sendmsg(q,line)
+});

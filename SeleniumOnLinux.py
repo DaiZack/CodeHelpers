4ebc0186726cf18a08d5ee0c3aef231d41479675
Zack Dai <45176371+DaiZack@users.noreply.github.com>
2020-07-08 18:26:29 -0400

    Update SeleniumOnLinux.py

--- SeleniumOnLinux.py
+++ SeleniumOnLinux.py
@@ -1,7 +1,14 @@
+#sudo apt-get install chromium-browser
+#pip install webdriver-manager
+
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
+
 # chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors
-driver = webdriver.Chrome(executable_path='/usr/lib/python3.6/chromedriver', chrome_options=chrome_options)
+# driver = webdriver.Chrome(executable_path='/usr/lib/python3.6/chromedriver', chrome_options=chrome_options)
+
+driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
 driver.get('https://python.org')
 print(driver.title)
